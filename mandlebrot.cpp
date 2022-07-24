c5b929afab799665c875e24b7f137f9bd3c08c1b
Robert Carr <70494103+RobertCarrUTA@users.noreply.github.com>
2022-07-23 20:19:16 -0500

    Can now generate a PPM file
    The program can now generate a PPM file, I am backing up my code cause I need to eat dinner.

--- mandlebrot.cpp
+++ mandlebrot.cpp
@@ -15,15 +15,12 @@
     --------------
     C++ reference: https://m.cplusplus.com/reference/
     --------------
-
     ------------------------------------------------------------------------------------------------------------------------------
     From https://en.wikipedia.org/wiki/Mandelbrot_set
     Just leaving this hear as a guide for myself, will remove it later
-
     In pseudocode, this algorithm would look as follows. The algorithm does not use complex numbers and manually simulates
     complex-number operations using two real numbers, for those who do not have a complex data type. The program may be simplified
     if the programming language includes complex-data-type operations.
-
     for each pixel (Px, Py) on the screen do
         x0 := scaled x coordinate of pixel (scaled to lie in the Mandelbrot X scale (-2.00, 0.47))
         y0 := scaled y coordinate of pixel (scaled to lie in the Mandelbrot Y scale (-1.12, 1.12))
@@ -46,12 +43,11 @@ int main ()
 {
     // Obtain the required inputs from the input file settings.txt (for easier modification by the user)
     std::ifstream settingsFile ("settings.txt");
+    int width, height, iterations, threads;
 
     if (settingsFile.is_open())
     {
         std::cout << "Opened settings.txt... Now reading the file...\n"<< std::endl;
-
-        int width, height, iterations, threads;
 
         // We need to get values of width, height, iterations, and threads
         while (settingsFile >> height >> width >> iterations >> threads)
@@ -74,13 +70,22 @@ int main ()
     }
     else std::cout << "Error: Unable to open settings.txt";
 
-    // Write the PPM header to the output file
-    
+    // This is how many results we should have (because a rectangular or square image is x * y pixels)
+    int values = height * width;
+
+    int color = 255;
+
+    // Create the PPM image with a file name and  to the output file
+    // For documentation on how to set this up, look here: http://netpbm.sourceforge.net/doc/ppm.html
+    std::ofstream fout("mandlebrot_image.ppm");
+    fout << "P3\n" << width << ' ' << height << ' ' << color << '\n'; // This could have been done on multiple lines but I decided to do it in one
+
+
+
     // For every pixel ...
     //   - Determine the real and imaginary numbers associated with pixel x and y on the image
     //   - Based on the previously found c value, find the number of iterations in the Mandelbrot formula
     //   - Map the RGB value of the resulting number
     //   - Output it to the image
-
 
 }
